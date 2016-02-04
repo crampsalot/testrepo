e6b84da3d5d82e88a6804148bea8cf1388c10344
Isa Hashim <isa@ultrablueberry.local>
2016-02-04 13:32:13 -0800

    Added dummy bg thread

--- TestGitHub/TestGitHub/ViewController.swift
+++ TestGitHub/TestGitHub/ViewController.swift
@@ -12,14 +12,39 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     @IBOutlet var imageView: UIImageView!
     
+    @IBOutlet weak var loadImageButton: UIButton!
+    
+    @IBOutlet weak var classifyButton: UIButton!
+    
     let imagePicker = UIImagePickerController()
     
+    @IBOutlet weak var textView: UITextView!
     
     @IBAction func loadImageButtonTapped(sender: UIButton) {
+        print("Load Image pressed")
+        
         imagePicker.allowsEditing = false
         imagePicker.sourceType = .PhotoLibrary
         
         presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func doClassify(sender: AnyObject) {
+        print("Classify pressed")
+        
+        loadImageButton!.enabled = false
+        
+        let priority = DISPATCH_QUEUE_PRIORITY_DEFAULT
+        dispatch_async(dispatch_get_global_queue(priority, 0)) {
+            // do some task
+            dispatch_async(dispatch_get_main_queue()) {
+                // update some UI
+                
+                self.loadImageButton!.enabled = true
+                
+                self.textView!.text = "Classify output from bg thread"
+            }
+        }
     }
     
     override func viewDidLoad() {
@@ -34,6 +59,8 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
             imageView.contentMode = .ScaleAspectFit
             imageView.image = pickedImage
+            
+            classifyButton!.enabled = true
         }
         
         dismissViewControllerAnimated(true, completion: nil)
